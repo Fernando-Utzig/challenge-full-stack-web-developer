5adc5fb5fa6df42ea17b19d8d8ebf7626b9e31bb
Fernando-Utzig <fernandoutzig@hotmail.com>
2022-01-26 10:53:30 -0300

    last update

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,8 +3,9 @@ import mysql from "mysql2";
 // create the connection to database
 const db = mysql.createConnection({
     host: 'localhost',
+    port: 3306,
     user: 'root',
-    password: '',
+    password: 'root',
     database: 'demo_db'
 });
 
