387fcdc641294b842e39cb3d09ae9af10e5a9300
Fernando-Utzig <fernandoutzig@hotmail.com>
2022-02-17 11:53:52 -0300

    Auth with Okta

--- app_vuejs/src/main.js
+++ app_vuejs/src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
+// The Vue build version to load with the `import` command
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import { createApp } from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+
+Vue.config.productionTip = false
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
+})
 
 createApp(App).mount('#app')
